eec9cdaf41658223bfca6c5684a9f178384f4c0b
Alexander Danilov <danilov.aa1@mail.ru>
2022-05-27 11:20:55 +0300

    1.2

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,37 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def omlet(request):
+    servings = request.GET.get('servings', None)
+    recipe = DATA['omlet']
+
+    context = {
+        'recipe': recipe,
+        'servings': servings if servings is None else int(servings),
+    }
+    return render(request, 'calculator/index.html', context)
+
+
+def pasta(request):
+    servings = request.GET.get('servings', None)
+    recipe = DATA['pasta']
+
+    context = {
+        'recipe': recipe,
+        'servings': servings if servings is None else int(servings),
+    }
+    return render(request, 'calculator/index.html', context)
+
+
+def buter(request):
+    servings = request.GET.get('servings', None)
+    recipe = DATA['buter']
+
+    context = {
+        'recipe': recipe,
+        'servings': servings if servings is None else int(servings),
+    }
+    return render(request, 'calculator/index.html', context)
