010ebf757f75f87a1e78d21dd6631c1248b8368f
Alexander Danilov <danilov.aa1@mail.ru>
2022-05-30 13:35:08 +0300

    add phones

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +9,26 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+
+    if request.GET.get('sort') == 'name':
+        phones = Phone.objects.all().order_by('name')
+    elif request.GET.get('sort') == 'min_price':
+        phones = Phone.objects.all().order_by('price')
+    elif request.GET.get('sort') == 'max_price':
+        phones = Phone.objects.all().order_by('-price')
+    else:
+        phones = Phone.objects.all()
+
+    context = {
+        'phones': phones
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.filter(slug=slug).first()
+    context = {
+        'phone': phone
+    }
     return render(request, template, context)
