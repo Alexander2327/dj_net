a488b3d7018d2f5552d4ce92831ef1bc16ceba2e
Alexander Danilov <danilov.aa1@mail.ru>
2022-05-30 15:23:00 +0300

    without date pagination

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from books.models import Book
+from django.core.paginator import Paginator
+
+
+def index(request):
+    return redirect('books')
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books = Book.objects.all()
+    context = {
+        'books': books
+    }
+    return render(request, template, context)
+
+
+def book_view(request, date):
+    template = 'books/books_list.html'
+    books = Book.objects.filter(pub_date=date).all()
+
+    paginator = Paginator(books, 10)
+    page_date = request.GET.get('date')
+    page = paginator.get_page(page_date)
+
+    context = {
+        'books': books,
+        'page': page,
+    }
     return render(request, template, context)
